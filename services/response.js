a9cc2de36afc49a85e6656a947b8bfb5792f8f85
notfinancialadvice <notfinancialadvice@notfinavicesMBP.lan>
2023-07-24 17:00:49 -0500

    more console.logs for webhooks1 fixing private reply6 quick replies try 3

--- services/response.js
+++ services/response.js
@@ -13,22 +13,6 @@
 const i18n = require("../i18n.config");
 
 module.exports = class Response {
-  // static genQuickReply(text, quickReplies) {
-  //   let response = {
-  //     text: text,
-  //     quick_replies: []
-  //   };
-
-  //   for (let quickReply of quickReplies) {
-  //     response["quick_replies"].push({
-  //       content_type: "text",
-  //       title: quickReply["title"],
-  //       payload: quickReply["payload"]
-  //     });
-  //   }
-
-  //   return response;
-  // }
   static genQuickReply(text, quickReplies) {
     let response = {
       text: text,
